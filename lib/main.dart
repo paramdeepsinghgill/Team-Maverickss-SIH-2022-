13b1ca7092c74d48a4f50febd8379b9459f0bbb9
Loki <uddeeptaraajkashyap@gmail.com>
2022-07-07 17:11:54 +0530

    basic str and navigator added

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import './pages/article_page.dart';
+import './pages/home_page.dart';
+import './pages/profile_page.dart';
+import './pages/test_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentIndex = 0;
+
+  final screens = [HomePage(), ArticlePage(), TestPage(), ProfilePage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: screens[currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              currentIndex: currentIndex,
+              selectedItemColor: Colors.pink,
+              unselectedItemColor: Colors.grey,
+              onTap: (int index) {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.auto_stories_outlined),
+                  label: 'Articles',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.assignment_turned_in_outlined),
+                  label: 'Tests',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  label: 'Profile',
+                ),
+              ]),
+        ));
+  }
+}
---
+++ lib/pages/article_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Article Page'),
+      ),
+    );
+  }
+}
