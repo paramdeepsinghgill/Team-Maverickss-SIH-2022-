63828daeb3ba80b8f965b7a38bfdadf7b62bda34
Shivam-Verma1 <107926305+Shivam-Verma1@users.noreply.github.com>
2022-07-11 17:14:44 +0530

    Update login_page.dart

--- login_page.dart
+++ login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:team-maverickss/utils/routes.dart';
+import 'package:team-maverickss/lib/routes.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
